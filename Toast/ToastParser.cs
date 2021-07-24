965193e5f9dacafaf30d5644f038d72b943b2fd0
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2021-07-24 10:32:21 +0900

    fix: command parameter exception

--- Toast/ToastParser.cs
+++ Toast/ToastParser.cs
@@ -118,12 +118,19 @@ namespace Toast
                 switch (nodes[index])
                 {
                     case CommandNode c:
-                        if ((isRight && c.Command.NamePosition != 0) || (!isRight && c.Command.NamePosition != c.Command.Parameters.Length))
+                        if (c.Parameters.Length > 0)
                         {
-                            throw new ParameterCountException();
+                            parameters.Add(c);
                         }
+                        else
+                        {
+                            if ((isRight && c.Command.NamePosition != 0) || (!isRight && c.Command.NamePosition != c.Command.Parameters.Length))
+                            {
+                                throw new ParameterCountException();
+                            }
 
-                        parameters.Add(new CommandNode(c.Command, GetParameters(nodes, index + (isRight ? 1 : -1), isRight, c.Command.Parameters.Length)));
+                            parameters.Add(new CommandNode(c.Command, GetParameters(nodes, index + (isRight ? 1 : -1), isRight, c.Command.Parameters.Length)));
+                        }
 
                         break;
                     case GroupNode g:
