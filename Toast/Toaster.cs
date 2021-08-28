fd60060de74c0f7e8e7d4d2587fbeb0729927f3d
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2021-08-28 11:12:32 +0900

    feat: change array to list

--- Toast/Toaster.cs
+++ Toast/Toaster.cs
@@ -16,7 +16,7 @@ namespace Toast
             { "text", typeof(string) },
             { "number", typeof(long) },
             { "float", typeof(float) },
-            { "array", typeof(object[]) },
+            { "list", typeof(object[]) },
         };
 
         public Toaster()
