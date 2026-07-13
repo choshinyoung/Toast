943e6f4ab27ab900cce144e84ec4afd14168fd7b
choshinyoung <choshinyoung1227@gmail.com>
2026-07-13 21:31:58 +0900

    ✨ Refactor interactive input handling in REPL for improved user experience

--- Toast.Interactive/Program.cs
+++ Toast.Interactive/Program.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Toast;
 
 Console.WriteLine("========================================");
@@ -10,14 +11,39 @@ toast.RegisterFunction("exit", (Context context) => Environment.Exit(0));
 
 while (true)
 {
-    Console.Write("toast> ");
+    var buffer = new StringBuilder();
+    string prompt = "toast> ";
+    bool eofReached = false;
 
-    var input = ReadLineInteractive(toast);
-    if (input == null)
+    while (true)
+    {
+        Console.Write(prompt);
+        var line = Console.ReadLine();
+        if (line == null)
+        {
+            eofReached = true;
+            break;
+        }
+
+        buffer.Append(line);
+
+        if (IsIncomplete(buffer.ToString(), toast))
+        {
+            buffer.Append('\n');
+            prompt = "       ";
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (eofReached && buffer.Length == 0)
     {
         break;
     }
 
+    var input = buffer.ToString();
     var trimmed = input.Trim();
     if (string.IsNullOrWhiteSpace(trimmed))
     {
@@ -57,104 +83,6 @@ while (true)
         Console.ResetColor();
         Console.WriteLine();
     }
-}
-
-static string? ReadLineInteractive(Toaster toast)
-{
-    if (Console.IsInputRedirected)
-    {
-        return Console.ReadLine();
-    }
-
-    var buffer = new System.Text.StringBuilder();
-
-    while (true)
-    {
-        ConsoleKeyInfo keyInfo;
-        try
-        {
-            keyInfo = Console.ReadKey(true);
-        }
-        catch (InvalidOperationException)
-        {
-            return Console.ReadLine();
-        }
-
-        if (keyInfo.Key == ConsoleKey.Enter)
-        {
-            if (IsIncomplete(buffer.ToString(), toast))
-            {
-                buffer.Append('\n');
-                Console.WriteLine();
-                Console.Write("       ");
-            }
-            else
-            {
-                Console.WriteLine();
-                break;
-            }
-        }
-        else if (keyInfo.Key == ConsoleKey.Backspace)
-        {
-            if (buffer.Length > 0)
-            {
-                char lastChar = buffer[buffer.Length - 1];
-                if (lastChar == '\n')
-                {
-                    buffer.Remove(buffer.Length - 1, 1);
-
-                    try
-                    {
-                        if (Console.CursorTop > 0)
-                        {
-                            Console.CursorLeft = 0;
-                            Console.Write("       "); // overwrite 7 spaces
-                            Console.CursorTop--;
-
-                            string content = buffer.ToString();
-                            int lastNewlineIdx = content.LastIndexOf('\n');
-                            string lastLine =
-                                lastNewlineIdx == -1
-                                    ? content
-                                    : content.Substring(lastNewlineIdx + 1);
-
-                            int promptLen = 7; // Both "toast> " and "       " are 7 characters
-                            Console.CursorLeft = promptLen + lastLine.Length;
-                        }
-                    }
-                    catch
-                    {
-                        // Ignore cursor movement errors in non-interactive / restricted consoles
-                    }
-                }
-                else
-                {
-                    buffer.Remove(buffer.Length - 1, 1);
-                    Console.Write("\b \b");
-                }
-            }
-        }
-        else if (keyInfo.Key == ConsoleKey.Tab)
-        {
-            buffer.Append("    ");
-            Console.Write("    ");
-        }
-        else
-        {
-            if (buffer.Length == 0 && (keyInfo.KeyChar == '\u001a' || keyInfo.KeyChar == '\u0004'))
-            {
-                return null;
-            }
-
-            if (keyInfo.KeyChar != '\0' && !char.IsControl(keyInfo.KeyChar))
-            {
-                buffer.Append(keyInfo.KeyChar);
-                Console.Write(keyInfo.KeyChar);
-            }
-        }
-    }
-
-    return buffer.ToString();
 }
 
 static bool IsIncomplete(string input, Toaster toast)
